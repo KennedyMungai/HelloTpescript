b01d7985f3448fb137e83fcef0604c0a95e8e568
KennedyMungai <kennedymungaifmab@gmail.com>
2022-06-07 13:54:03 +0300

    Code generated aftere compilation

--- dist/Index.js
+++ dist/Index.js
@@ -1,5 +1,7 @@
 "use strict";
-;
-let mySize = 1;
-console.log(mySize);
+function getCustomer(id) {
+    return id === 0 ? null : { birthday: new Date() };
+}
+let customer = getCustomer(1);
+console.log(customer === null || customer === void 0 ? void 0 : customer.birthday);
 //# sourceMappingURL=Index.js.map
